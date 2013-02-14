56660f7ad7684106a477417a2794f234bf02f836
july <tangyike455665@gmail.com>
2013-02-14 14:19:47 -0500

    update step_gibbs.py

--- code/step_gibbs.py
+++ code/step_gibbs.py
@@ -1,3 +1,4 @@
+#Gibbs sampler written iin the form of python class
 import random
 import cmath
 import math
@@ -34,7 +35,7 @@ def jacobian(e,e0,g):
 	J4=(reshear(e+delt,g).imag-y)/delt
 	return np.abs(J1*J2-J3*J4)
 def prior(x):
-	return -0.0*np.sum((x[1:]-x[:-1])**2)
+	return -2.0*np.sum((x[1:]-x[:-1])**2)
 def fep(x,alpha,beta):
 	if x<0 or x>1:
 		return 0
@@ -69,15 +70,55 @@ def postfunc(X,E):
 	result[0]=np.sum(result[1:])+prior(P)
 #	result[0]=np.sum(result[1:])
 	return result
-random.seed(93)
-np.random.seed(seed=89)
+class Gibbssampler(function,argus):
+	def __init__(self,function,argus,X0):
+		self.function=function
+		self.argus=argus
+	def run(self,X,prosig,nsteps,Seed=None):
+		np.random.seed(seed=Seed)
+		self.value=sel.function(X,*self.argus)
+		self.pos=1.0*X
+		X1=1.0*X
+		u=np.zeros(NP)
+		self.Chain=np.zeros((Nsteps,ndim))
+		accnum1=0.0
+		accnum2=0.0
+		for i in xrange(nsteps):
+			X1[Nbin:]=self.pos[Nbin:]
+			X1[:Nbin]=self.pos[:Nbin]+np.random.normal(loc=0.0,scale=prosig[:Nbin])
+			fvalue1=self.function(X1,*self.argus)
+			lnprob[0]=fvalue1[0]-self.value[0]
+			m=math.log(random.random())
+			if m<lnprob[0]:
+				accnum1+=1
+				self.pos[:Nbin]=X1[:Nbin]
+				self.value[:]=fvalue1[:]
+			X1[:Nbin]=self.pos[:Nbin]
+			X1[Nbin:]=self.pos[Nbin:]+np.random.normal(loc=0.0,scale=prosig[Nbin:])
+			fvalue1[:]=self.function(X1,*self.argus)
+			lnprob[1:]=fvalue1[1:]-self.value[1:]
+			u[:]=np.log(np.random.rand(NP))
+			self.pos[Nbin::2][u<lnprob[1:]]=X1[Nbin::2][u<lnprob[1:]]
+			self.pos[Nbin+1::2][u<lnprob[1:]]=X1[Nbin+1::2][u<lnprob[1:]]
+			temp=np.sum(self.value[1:])
+			self.value[1:][u<lnprob[1:]]=fvalue1[1:][u<lnprob[1:]]
+			self.value[0]+=np.sum(self.value[1:])-temp
+			accnum2+=1.0*len(u[u<lnprob[1:]])/NP
+			self.Chain[i]=self.pos
+		self.accptrate1=1.0*accnum1/nsteps
+		self.accptrate2=accnum2/nsteps
+		return self.pos
+		
+			
+#random.seed(93)
+#np.random.seed(seed=75)
 i=0
-p=2.8
-q=2.8
+p,q=2.8,2.8
 phi=np.random.rand(N*NP)
 epsilon=np.random.beta(p,q,size=N*NP)
 elipse=epsilon*np.exp(2J*phi*math.pi)
-gamma=0.1*(1-2*np.random.rand(NP))+0.1*(1J*(1-2*np.random.rand(NP)))
+#gamma=0.1*(1-2*np.random.rand(NP))+0.1*(1J*(1-2*np.random.rand(NP)))
+gamma=np.random.normal(scale=0.05,size=NP)+1J*np.random.normal(scale=0.05,size=NP)
 E=shear(elipse,gamma[index])
 f=file("gamma.txt","w")
 for i in range(NP):
@@ -95,65 +136,31 @@ average[1::2]=np.imag(ave)
 E.shape=(NP*N)
 LIN=1.0/Nbin*np.linspace(0,Nbin-1,Nbin)+0.5/Nbin
 P0=np.log(np.histogram(np.abs(E),bins=Nbin, range=((0,1)), normed=True)[0]/LIN)
-
 #print(average,abs(complex(average[0],average[1])-gamma[0]))
 #P0=np.array((2.8,2.8))
 X0=initial(P0,average) 
-#print(X0)
 prosig=np.zeros(2*NP+Nbin)
 prosig[Nbin:]=0.13
-prosig[:Nbin]=0.08
-
-np.random.seed(seed=None)
-def Gibbssampler(X,function,E,prosig,Nsteps):
-	X0=1.0*X
-	fary0=function(X0,E)
-	Chain=np.zeros((Nsteps,ndim))
-	accnum1=0.0
-	accnum2=0.0
-#	logpost=np.zeros(Nsteps)
-	X1=np.zeros(len(X0))
-	X1[:]=X0[:]
-	lnprob=np.zeros(NP+1)
-	for i in xrange(Nsteps):
-		X1[Nbin:]=X0[Nbin:]
-		X1[:Nbin]=X0[:Nbin]+np.random.normal(loc=0.0,scale=prosig[:Nbin])
-		fary1=function(X1,E)
-		lnprob[0]=fary1[0]-fary0[0]
-#		print(fary1[0],fary0[0])
-#		print(function(X0,E)[0])
-		u=math.log(random.random())
-		if u<lnprob[0]:
-			accnum1+=1
-			X0[:Nbin]=X1[:Nbin]
-			fary0[:]=fary1[:]
-		X1[:Nbin]=X0[:Nbin]
-		X1[Nbin:]=X0[Nbin:]+np.random.normal(loc=0.0,scale=prosig[Nbin:])
-		fary1=function(X1,E)
-		lnprob[1:]=fary1[1:]-fary0[1:]
-		u=np.log(np.random.rand(NP))
-		X0[Nbin::2][u<lnprob[1:]]=X1[Nbin::2][u<lnprob[1:]]
-		X0[Nbin+1::2][u<lnprob[1:]]=X1[Nbin+1::2][u<lnprob[1:]]
-		temp=np.sum(fary0[1:])
-		fary0[1:][u<lnprob[1:]]=fary1[1:][u<lnprob[1:]]
-		fary0[0]+=np.sum(fary0[1:])-temp
-		accnum2+=1.0*len(u[u<lnprob[1:]])/NP
-#		logpost[i]=fary0[0]
-		Chain[i]=X0
-	print(1.0*accnum1/Nsteps)
-	print(1.0*accnum2/Nsteps)
-#	f=file("logpost.txt","w")
-#	for i in range(len(logpost[::10])):
-#		f.write("%f\n" %logpost[::10][i])
-#	return Chain.transpose()
-	return Chain
-Chain=Gibbssampler(X0,postfunc,E,prosig,2000)
-Chain1=Gibbssampler(Chain[1999],postfunc,E,prosig,22000)
+prosig[:Nbin]=0.06
+sampler=Gibbssampler(postfunc,[E])
+pos=sampler.run(X0,prosig,2000)
+pos=sampler.run(pos,prosig,22000)
+Chain=sampler.Chain
 result=np.array([1+1J for i in xrange(NP)])
 for i in xrange(NP):
-	result[i]=complex(np.mean(Chain1.transpose()[2*i+Nbin]),np.mean(Chain1.transpose()[2*i+Nbin+1]))
+	result[i]=complex(np.mean(Chain.transpose()[2*i+Nbin]),np.mean(Chain.transpose()[2*i+Nbin+1]))
 print(np.mean(np.abs(result-gamma)**2)**0.5)
 print(np.mean(np.abs(ave-gamma)**2)**0.5)
+
+
+
+
+
+
+
+                                #************************plot***********************#
+e0=reshear(E,result[index])
+W=np.histogram(np.abs(e0),bins=Nbin, range=((0,1)), normed=True)[0]
 #print(ave-gamma)
 #print(np.mean(Chain1.transpose()[2*NP+1]),np.std(Chain1.transpose()[2*NP+1]),gamma[NP-1].imag)
 P=np.zeros(Nbin)
@@ -161,14 +168,21 @@ for i in xrange(Nbin):
 	P[i]=np.mean(Chain1.transpose()[i])
 P=P-math.log(np.sum(np.exp(P)*(1.0/Nbin*np.linspace(0,Nbin-1,Nbin)+0.5/Nbin)))
 P=np.exp(P)*(1.0/Nbin*np.linspace(0,Nbin-1,Nbin)+0.5/Nbin)
-X=np.log(np.histogram(epsilon,bins=Nbin, range=((0,1)), normed=True)[0]/LIN)
-Q=X[:Nbin]-math.log(np.sum(np.exp(X[:Nbin])*(1.0/Nbin*np.linspace(0,Nbin-1,Nbin)+0.5/Nbin)))
-Q=np.exp(Q)*(1.0/Nbin*np.linspace(0,Nbin-1,Nbin)+0.5/Nbin)
-
+P=P/np.sum(P)*10
+Q=np.histogram(epsilon,bins=Nbin, range=((0,1)), normed=True)[0]
+Q=Q/np.sum(Q)*10
 a=1.0/Nbin*np.linspace(0,Nbin-1,Nbin)+0.5/Nbin
+#plt.subplot(211)
 plt.scatter(a,P)
+plt.scatter(a,W,color='g')
 plt.scatter(a,Q,color='r')
-plt.ylim((0,0.4))
+#plt.hist(np.abs(e0), bins=10, range=(0,1), normed=True,color='g')
+plt.ylim((0,2.5))
+plt.xlim((0,1))
+#plt.subplot(212)
+#plt.hist(np.abs(e0), bins=20, range=(0,1), normed=True,color='g')
+#plt.ylim((0,2.5))
+#plt.xlim((0,1))
 plt.show()
 
 #import matplotlib.pyplot as pl
